60b963bb1a87a5df3085673918e7bcd3978eb5e2
cokekishan@gmail.com <Krishna.Pk@VDESNTCS1046.rci.rogers.ca>
2018-03-06 08:23:00 -0500

    New Project

---
+++ WorkshopJ9/src/com/test/pack/Test1.java
@@ -0,0 +1,21 @@
+package com.test.pack;
+
+import java.util.Scanner;
+
+public class Test1 extends Test2 {
+
+    public static void main(String[] args) {
+/*        Scanner scan = new Scanner(System.in);
+        String s ="He is a very very good boy, isn't he?";
+        // Write your code here.
+        scan.close();*/
+    	
+    	Test1 ts = new Test1();
+    	ts.dummyMethod();
+    }
+    
+    void dummyMethod() {
+    	System.out.println("Test1");
+    }
+
+}
---
+++ WorkshopJ9/src/com/test/pack/Test2.java
@@ -0,0 +1,14 @@
+package com.test.pack;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	void dummyMethod() {
+		System.out.println("Dummy Method ");
+	}
+}
